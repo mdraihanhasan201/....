f1777efa8b75f7a2bff2c4da5b5bbfed23e15755
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-02-20 23:59:42 +0600

    Add files via upload
    short_hand_if_else_conditions

---
+++ short_hand.java
@@ -0,0 +1,12 @@
+package if_else;
+
+public class short_hand {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+
+        String result = (a > b) ? "good day" : "good evening";
+        System.out.println(result);
+
+    }
+}
