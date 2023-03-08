226ad5880f941b47008b3751fbab5694a6cc461e
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-03-08 21:30:41 +0600

    Add files via upload

---
+++ loop/f.java
@@ -0,0 +1,25 @@
+// package loop;
+
+// public class f {
+//     public static void main(String[] args) {
+
+//         int n = 5;
+//         // for loop  
+//         for (int i = 1; i <= n; ++i) {
+//           System.out.println("Java is fun");
+//         }
+//       }
+// }
+
+package loop;
+
+public class f {
+    public static void main(String[] args) {
+
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println(i);
+        }
+    }
+}
