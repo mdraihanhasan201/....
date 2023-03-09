d4fed448ed42a74a367d4213ce4f80d408ded926
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-03-09 22:16:04 +0600

    Add files via upload
    using break ststement

---
+++ loop/b.java
@@ -0,0 +1,36 @@
+package loop;
+
+public class b {
+
+
+
+
+    import java.util.Scanner;
+
+class UserInputSum {
+    public static void main(String[] args) {
+      
+        Double number, sum = 0.0;
+
+        // create an object of Scanner
+        Scanner input = new Scanner(System.in);
+      
+        while (true) {
+            System.out.print("Enter a number: ");
+
+            // takes double input from user
+            number = input.nextDouble();
+         
+            // if number is negative the loop terminates
+            if (number < 0.0) {
+                break;
+            }
+         
+           sum += number;
+        }
+        System.out.println("Sum = " + sum);
+    }
+    
+}
+
+
